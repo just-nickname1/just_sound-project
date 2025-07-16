226bfe564052487be632c332f94486660baf40de
just-nickname1 <wonjin120310@naver.com>
2025-07-16 14:04:36 +0900

    pxt.json, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,20 +1,18 @@
-input.onSound(DetectedSound.Loud, function on_sound_loud() {
-    
+input.onSound(DetectedSound.Loud, function () {
     작동_시작 += 1
 })
 let 작동_시작 = 0
 input.setSoundThreshold(SoundThreshold.Loud, 250)
 music._playDefaultBackground(music.builtInPlayableMelody(Melodies.PowerUp), music.PlaybackMode.InBackground)
-basic.forever(function on_forever() {
+basic.forever(function () {
     if (작동_시작 == 1) {
         basic.pause(1000)
         music._playDefaultBackground(music.builtInPlayableMelody(Melodies.JumpUp), music.PlaybackMode.InBackground)
         basic.pause(2000)
         music._playDefaultBackground(music.builtInPlayableMelody(Melodies.JumpDown), music.PlaybackMode.InBackground)
     }
-    
 })
-basic.forever(function on_forever2() {
+basic.forever(function () {
     if (작동_시작 == 1) {
         basic.showLeds(`
             # . # . .
@@ -38,5 +36,4 @@ basic.forever(function on_forever2() {
             . # . # .
             `)
     }
-    
 })
