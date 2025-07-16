2190616d26c94eaa3d6e942f32674fe2abe6aea3
just-nickname1 <wonjin120310@naver.com>
2025-07-16 14:04:14 +0900

    pxt.json, main.blocks, main.ts, main.py 업데이트하기

--- main.py
+++ main.py
@@ -1,8 +1,44 @@
+def on_sound_loud():
+    global 작동_시작
+    작동_시작 += 1
+input.on_sound(DetectedSound.LOUD, on_sound_loud)
+
 작동_시작 = 0
 input.set_sound_threshold(SoundThreshold.LOUD, 250)
 music._play_default_background(music.built_in_playable_melody(Melodies.POWER_UP),
     music.PlaybackMode.IN_BACKGROUND)
 
 def on_forever():
-    pass
+    if 작동_시작 == 1:
+        basic.pause(1000)
+        music._play_default_background(music.built_in_playable_melody(Melodies.JUMP_UP),
+            music.PlaybackMode.IN_BACKGROUND)
+        basic.pause(2000)
+        music._play_default_background(music.built_in_playable_melody(Melodies.JUMP_DOWN),
+            music.PlaybackMode.IN_BACKGROUND)
 basic.forever(on_forever)
+
+def on_forever2():
+    if 작동_시작 == 1:
+        basic.show_leds("""
+            # . # . .
+            # # # # #
+            . . # . #
+            . # . # #
+            . # . . .
+            """)
+        basic.show_leds("""
+            . . # . #
+            # # # # #
+            # . # . .
+            # # . # .
+            . . . # .
+            """)
+        basic.show_leds("""
+            # . # . #
+            . # # # .
+            . . # . .
+            . # . # .
+            . # . # .
+            """)
+basic.forever(on_forever2)
--- main.ts
+++ main.ts
@@ -1,14 +1,42 @@
-input.onSound(DetectedSound.Loud, function () {
+input.onSound(DetectedSound.Loud, function on_sound_loud() {
+    
     작동_시작 += 1
 })
 let 작동_시작 = 0
 input.setSoundThreshold(SoundThreshold.Loud, 250)
 music._playDefaultBackground(music.builtInPlayableMelody(Melodies.PowerUp), music.PlaybackMode.InBackground)
-basic.forever(function () {
+basic.forever(function on_forever() {
     if (작동_시작 == 1) {
         basic.pause(1000)
         music._playDefaultBackground(music.builtInPlayableMelody(Melodies.JumpUp), music.PlaybackMode.InBackground)
         basic.pause(2000)
         music._playDefaultBackground(music.builtInPlayableMelody(Melodies.JumpDown), music.PlaybackMode.InBackground)
     }
+    
+})
+basic.forever(function on_forever2() {
+    if (작동_시작 == 1) {
+        basic.showLeds(`
+            # . # . .
+            # # # # #
+            . . # . #
+            . # . # #
+            . # . . .
+            `)
+        basic.showLeds(`
+            . . # . #
+            # # # # #
+            # . # . .
+            # # . # .
+            . . . # .
+            `)
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            . . # . .
+            . # . # .
+            . # . # .
+            `)
+    }
+    
 })
