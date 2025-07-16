d1dad25e857bf0f70dbd80e34c976ea67463c3e2
just-nickname1 <wonjin120310@naver.com>
2025-07-16 14:01:44 +0900

    pxt.json, main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ let 작동_시작 = 0
 input.setSoundThreshold(SoundThreshold.Loud, 250)
 music._playDefaultBackground(music.builtInPlayableMelody(Melodies.PowerUp), music.PlaybackMode.InBackground)
 basic.forever(function () {
-    if ((0 as any) == (1 as any)) {
+    if (작동_시작 == 1) {
         basic.pause(1000)
         music._playDefaultBackground(music.builtInPlayableMelody(Melodies.JumpUp), music.PlaybackMode.InBackground)
         basic.pause(2000)
